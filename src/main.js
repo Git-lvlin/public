dc92342a25205169aa917f4ee818feb465369dc5
wuhailing <573058815@qq.com>
2021-06-01 11:33:28 +0800

    调试

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue';
 import Big from 'big.js';
+import VConsole from 'vconsole';
 import App from './App';
 import router from './router';
 import store from './store';
+
+const vconsole = new VConsole();
 
 Vue.config.productionTip = false;
 Vue.filter('price', (value) => new Big(value).div(new Big(100)));
@@ -12,3 +15,5 @@ new Vue({
   store,
   render: (h) => h(App),
 }).$mount('#app');
+
+export default vconsole;
