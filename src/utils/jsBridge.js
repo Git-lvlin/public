6cbd8891669b96d9c03484d1738ac261a14f5ef8
wuhailing <573058815@qq.com>
2021-06-04 21:55:36 +0800

    jsBridge

--- src/utils/jsBridge.js
+++ src/utils/jsBridge.js
@@ -40,13 +40,13 @@ function setupWebViewJavascriptBridge(callback) {
       return window.WVJBCallbacks.push(callback);
     }
     window.WVJBCallbacks = [callback];
-    // const WVJBIframe = document.createElement('iframe');
-    // WVJBIframe.style.display = 'none';
-    // WVJBIframe.src = 'wvjbscheme://__BRIDGE_LOADED__';
-    // document.documentElement.appendChild(WVJBIframe);
-    // setTimeout(() => {
-    //   document.documentElement.removeChild(WVJBIframe);
-    // }, 0);
+    const WVJBIframe = document.createElement('iframe');
+    WVJBIframe.style.display = 'none';
+    WVJBIframe.src = 'wvjbscheme://__BRIDGE_LOADED__';
+    document.documentElement.appendChild(WVJBIframe);
+    setTimeout(() => {
+      document.documentElement.removeChild(WVJBIframe);
+    }, 0);
   }
 }
 setupWebViewJavascriptBridge((bridge) => {
