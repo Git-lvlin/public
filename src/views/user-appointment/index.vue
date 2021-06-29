b9b6c9985e310a16320e9a4f81a22b044e32ee42
wuhailing <573058815@qq.com>
2021-06-29 20:29:51 +0800

    资源位

--- src/views/user-appointment/index.vue
+++ src/views/user-appointment/index.vue
@@ -74,9 +74,12 @@ export default {
         resourceKey: "MINIEXAMINE",
         timeVersion: new Date().getTime(),
       }).then(res => {
-        console.log("res", res);
-        if(!!res.state) {
+        // console.log("res", res);
+        if(res.code == 0) {
+          const data = res.data.data;
+        if(!!data.state) {
           this.getUserList()
+        }
         }
       });
 
