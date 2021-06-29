5483c10afa946ea10ad09dfaae17c15db9f54118
wuhailing <573058815@qq.com>
2021-06-29 17:29:23 +0800

    store

--- src/App.vue
+++ src/App.vue
@@ -7,20 +7,24 @@
 </template>
 
 <script>
-const {
-  appInfo,
-} = this.$store.state;
-if (!window.WeixinJSBridge || !window.WeixinJSBridge.invoke) {
-  document.addEventListener('WeixinJSBridgeReady', () => {
-    if (window.__wxjs_environment === 'miniprogram') {
-      appInfo.isMiniprogram = true;
-    } else {
-      appInfo.isMiniprogram = false;
+export default {
+  created() {
+    const {
+      appInfo,
+    } = this.$store.state;
+    if (!window.WeixinJSBridge || !window.WeixinJSBridge.invoke) {
+      document.addEventListener('WeixinJSBridgeReady', () => {
+        if (window.__wxjs_environment === 'miniprogram') {
+          appInfo.isMiniprogram = true;
+        } else {
+          appInfo.isMiniprogram = false;
+        }
+        this.$store.commit("updateAppInfo", appInfo);
+        console.log("this.$store", this)
+      }, false);
     }
-    this.$store.commit("updateAppInfo", appInfo); 
-  }, false);
+  },
 }
-
 </script>
 
 <style lang="scss">
