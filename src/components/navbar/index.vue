90cc9e261ff62a2040199fa6764fe7b8683b173a
wuhailing <573058815@qq.com>
2021-06-29 19:16:58 +0800

    返回

--- src/components/navbar/index.vue
+++ src/components/navbar/index.vue
@@ -8,7 +8,7 @@
       :safe-area-inset-top="true"
       @click-left="onClickLeft"
     >
-      <template #left v-if="!isMiniprogram">
+      <template #left v-if="false">
         <img
           class="back_img"
           :src="getImgUrl('miniprogram/common/back_white@2x.png')"
