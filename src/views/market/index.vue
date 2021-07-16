b92381c18ca4ecb60daac1ae8646696cdac1d8ae
huyueke <huyueke@yongdaxing.cn>
2021-07-16 22:08:11 +0800

    新增营销落地页

---
+++ src/views/market/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="store-in">
+    <list v-for="item in list" :key="item" :good="item" />
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import { Image as VanImage } from 'vant';
+import { getImgUrl } from '@/utils/tools';
+import List from './components/list';
+Vue.use(VanImage);
+export default {
+  props: {
+    list: {
+      type: Array,
+      default: []
+    },
+  },
+  data() {
+    return {
+      list: []
+    };
+  },
+  components: {
+    List,
+  },
+  created () {
+  },
+  methods: {
+    getImgUrl,
+  },
+  mounted() {
+
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  .store-in {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
+</style>
