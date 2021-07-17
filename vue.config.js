dfa58299c8a2bd7e23cbc1542bf6f5a24da395b7
huyueke <huyueke@yongdaxing.cn>
2021-07-17 15:12:31 +0800

    change about unitprecision

--- vue.config.js
+++ vue.config.js
@@ -41,7 +41,7 @@ module.exports = {
           new PxToVw({
             unitToConvert: 'px', // 需要转换的单位，默认为"px"；
             viewportWidth: 375, // 设计稿的视口宽度
-            unitPrecision: 5, // 单位转换后保留的小数位数
+            unitPrecision: 6, // 单位转换后保留的小数位数
             propList: ['*'], // 要进行转换的属性列表,*表示匹配所有,!表示不转换
             viewportUnit: 'vw', // 转换后的视口单位
             fontViewportUnit: 'vw', // 转换后字体使用的视口单位
