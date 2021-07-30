699d52cd230fda834fa678d3657b91a346fdfd36
huyueke <huyueke@yongdaxing.cn>
2021-07-30 15:08:32 +0800

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -132,7 +132,7 @@ const router = new VueRouter({
 });
 
 router.beforeEach((to, from, next) => {
-  document.title = to.meta.title || '约购';
+  document.title = to.meta.title || '';
   next();
 });
 
