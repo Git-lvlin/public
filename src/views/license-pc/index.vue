18a07267f3557ac60f88ebeb19d507818a1ccaae
RogersHuang <>
2021-10-08 15:03:56 +0800

    modify:修改证照资质页面

--- src/views/license-pc/index.vue
+++ src/views/license-pc/index.vue
@@ -1,29 +1,37 @@
 <template>
   <div class="license-pc">
     <div class="box">
-      <van-image width="30%" height="30%" :src="getImgUrl('publicMobile/license/yyzz.jpg')"/>
-      <van-image width="30%" height="30%" :src="getImgUrl('publicMobile/license/zzdx.jpg')"/>
+      <van-image
+        width="30%"
+        height="30%"
+        :src="getImgUrl('publicMobile/license/yyzz.png')"
+      />
+      <van-image
+        width="30%"
+        height="30%"
+        :src="getImgUrl('publicMobile/license/zzdx.png')"
+      />
+      <van-image
+        width="30%"
+        height="30%"
+        :src="getImgUrl('publicMobile/license/zzdx-2.png')"
+      />
     </div>
   </div>
 </template>
 
 <script>
-import Vue from 'vue';
-import { Image as VanImage } from 'vant';
-import { getImgUrl } from '@/utils/tools';
+import Vue from "vue";
+import { Image as VanImage } from "vant";
+import { getImgUrl } from "@/utils/tools";
 Vue.use(VanImage);
 export default {
-  props: {
-
-  },
+  props: {},
   data() {
-    return {
-    };
+    return {};
   },
-  components: {
-  },
-  created () {
-  },
+  components: {},
+  created() {},
   methods: {
     getImgUrl,
   },
@@ -31,15 +39,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .license-pc {
-    min-height: 100vh;
-    background-color: #ffffff;
-    overflow: hidden;
-    overflow-y: auto;
-    .box {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-    }
+.license-pc {
+  min-height: 100vh;
+  background-color: #ffffff;
+  overflow: hidden;
+  overflow-y: auto;
+  .box {
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
+}
 </style>
--- src/views/license/index.vue
+++ src/views/license/index.vue
@@ -1,27 +1,23 @@
 <template>
   <div class="license">
-    <van-image :src="getImgUrl('publicMobile/license/yyzz.jpg')"/>
-    <van-image :src="getImgUrl('publicMobile/license/zzdx.jpg')"/>
+    <van-image :src="getImgUrl('publicMobile/license/yyzz.png')" />
+    <van-image :src="getImgUrl('publicMobile/license/zzdx.png')" />
+    <van-image :src="getImgUrl('publicMobile/license/zzdx-2.png')" />
   </div>
 </template>
 
 <script>
-import Vue from 'vue';
-import { Image as VanImage } from 'vant';
-import { getImgUrl } from '@/utils/tools';
+import Vue from "vue";
+import { Image as VanImage } from "vant";
+import { getImgUrl } from "@/utils/tools";
 Vue.use(VanImage);
 export default {
-  props: {
-
-  },
+  props: {},
   data() {
-    return {
-    };
+    return {};
   },
-  components: {
-  },
-  created () {
-  },
+  components: {},
+  created() {},
   methods: {
     getImgUrl,
   },
@@ -29,10 +25,10 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .license {
-    display: flex;
-    flex-direction: column;
-    min-height: 100vh;
-    background-color: #ffffff;
-  }
+.license {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  background-color: #ffffff;
+}
 </style>
