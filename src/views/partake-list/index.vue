15dcf84eadc44e10de73f3aa7bf4abfcb69179ae
huyueke <huyueke@yongdaxing.cn>
2021-11-03 14:34:07 +0800

    fix

--- src/views/partake-list/index.vue
+++ src/views/partake-list/index.vue
@@ -50,9 +50,21 @@ export default {
   },
   methods: {
     getImgUrl,
+    timestampToTime(timestamp) {
+      var date = new Date(timestamp*1000);//时间戳为10位需*1000，时间戳为13位的话不需乘1000
+      var Y = date.getFullYear() + '/';
+      var M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '/';
+      var D = (date.getDate() < 10 ? '0'+date.getDate() : date.getDate()) + ' ';
+      return Y+M+D;
+    },
     getList() {
       teamApi.getstoreransList({next:0,size:99}, {token:this.token}).then((res) => {
-        this.listData = res.data.records
+        this.listData = res.data.records.map(item => {
+          return {
+            ...item,
+            finalPaymentTime: this.timestampToTime(item.finalPaymentTime)
+          }
+        })
       })
     },
     getUserInfo() {
