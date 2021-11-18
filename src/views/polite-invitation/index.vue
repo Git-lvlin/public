511f0ccc54b1f6f53940ea5b285501f7fac1eff1
huyueke <huyueke@yongdaxing.cn>
2021-11-18 16:09:41 +0800

    fix

---
+++ src/views/polite-invitation/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    <van-image
+      class="border"
+      width="26px"
+      height="4px"
+      :src="getImgUrl('publicMobile/share/border.png')"
+    />
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import { Image as VanImage } from 'vant';
+import { getImgUrl } from '@/utils/tools';
+Vue.use(VanImage);
+export default {
+  data() {
+    return {
+    };
+  },
+  created () {
+  },
+  mounted() {
+  },
+  methods: {
+    getImgUrl,
+  },
+};
+</script>
+
+<style lang='scss' scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  background-color: #ffffff;
+}
+</style>
