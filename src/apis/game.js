b28b87462675dea28bcc5a6abba12fd02fc4e9d0
wuhailing <573058815@qq.com>
2022-01-10 19:00:21 +0800

    列表&&提现

--- src/apis/game.js
+++ src/apis/game.js
@@ -4,7 +4,14 @@ const url = {
   game: '/activity/auth/buildhouseInfo',
   pkInvite: '/activity/auth/getInviteRank',
   pkRank: '/activity/auth/getFloorRank',
+  rankList: '/activity/auth/getMemberBuildhouseList',
+  totalPrice: '/activity/auth/getMyTotalPrize',
+  priceList: '/activity/auth/getMyPrizeList',
   accountInfo: '/financial/auth/purseAccountActivity/info',
+  withdrawVerify: '/financial/auth/purseAccountActivity/withdrawVerifyAmount',
+  withdrawSms: '/financial/auth/purseAccountActivity/withdrawSendSms',
+  withdrawApply: '/financial/auth/purseAccountActivity/withdrawApply',
+  withdrawList: '/financial/auth/purseAccountActivity/rollPage',
   demo: '/activity/auth/useBhDemoChance',
   pro: '/activity/auth/useBuildhouseChance',
   prize: '/activity/auth/prizeDraw',
@@ -47,6 +54,7 @@ export default {
       options,
     });
   },
+  // 排行榜 邀请列表
   getPkInvite(params = {}, options = {}) {
     return post({
       url: url.pkInvite,
@@ -54,6 +62,7 @@ export default {
       options,
     });
   },
+  // 排行榜 楼层列表
   getPkRank(params = {}, options = {}) {
     return post({
       url: url.pkRank,
@@ -61,6 +70,31 @@ export default {
       options,
     });
   },
+  // 机会明细列表
+  getRankList(params = {}, options = {}) {
+    return post({
+      url: url.rankList,
+      data: params,
+      options,
+    });
+  },
+  // 参与记录 红包总额
+  getTotalPrice(params = {}, options = {}) {
+    return post({
+      url: url.totalPrice,
+      data: params,
+      options,
+    });
+  },
+  // 参与记录 红包列表
+  getPriceList(params = {}, options = {}) {
+    return post({
+      url: url.priceList,
+      data: params,
+      options,
+    });
+  },
+  // 提现账户信息
   getAccountInfo(params = {}, options = {}) {
     return post({
       url: url.accountInfo,
@@ -68,5 +102,36 @@ export default {
       options,
     });
   },
-  
+  // 提现金额校验
+  getWithdrawVerify(params = {}, options = {}) {
+    return post({
+      url: url.withdrawVerify,
+      data: params,
+      options,
+    });
+  },
+  // 提现发送验证码
+  getWithdrawSms(params = {}, options = {}) {
+    return post({
+      url: url.withdrawSms,
+      data: params,
+      options,
+    });
+  },
+  // 提现申请
+  getWithdrawApply(params = {}, options = {}) {
+    return post({
+      url: url.withdrawApply,
+      data: params,
+      options,
+    });
+  },
+  // 提现列表
+  getWithdrawList(params = {}, options = {}) {
+    return post({
+      url: url.withdrawList,
+      data: params,
+      options,
+    });
+  },
 };
--- src/views/game-join-history/index.vue
+++ src/views/game-join-history/index.vue
@@ -1,144 +1,286 @@
 <template>
   <div class="history-box">
-    <div class="bar-list">
+    <div class="flex_fix bar-list">
       <div :class="`bar-item ${actType == 1 ? 'bar-act' : ''}`" @click="onChangeBar(1)">获得机会</div>
       <div :class="`bar-item ${actType == 2 ? 'bar-act' : ''}`" @click="onChangeBar(2)">我的红包奖励</div>
     </div>
 
     <div class="history-content">
-      <!-- 获得机会 -->
-      <div class="opportunity-content" v-if="actType == 1">
-        <div class="flex_middle opportunity-time" @click="showDate = true">
-          2020年11月
-          <div class="tap-bottom" />
-        </div>
+      <List
+        :finished="listFinished"
+        style="height: 100%"
+        @load="handleBottom"
+        :offset="0"
+      >
+        <!-- 获得机会 -->
+        <div class="opportunity-content" v-if="actType == 1 && list.length">
+          <!-- <div class="flex_middle opportunity-time" @click="showDate = true">
+            2020年11月
+            <div class="tap-bottom" />
+          </div> -->
 
-        <div class="opportunity-list">
-          <div class="flex_between list-item">
-            <div class="list-info">
-              <div>邀请188****8888新用户注册</div>
-              <div class="list-info-time">2021-01-01 10:00:00</div>
+          <div class="opportunity-list">
+            <div class="flex_between list-item" v-for="item in list">
+              <div class="list-info">
+                <div>{{item.title}}</div>
+                <div class="list-info-time">{{item.createTime}}</div>
+              </div>
+              <div :class="`list-num ${item.transferType == 2 ? 'list-num-def' : ''}`">
+                游戏机会{{`${item.transferType == 1 ? '+' : '-'}${item.num}`}}
+              </div>
             </div>
-            <div class="list-num">游戏机会+1</div>
+            <div class="no-more" v-if="!rankPage.hasNext">暂无更多~</div>
           </div>
-          <div class="no-more">暂无更多~</div>
+
+          <Popup
+            v-model="showDate"
+            position="bottom"
+          >
+            <DatetimePicker
+              v-model="currentDate"
+              type="year-month"
+              title="选择年月"
+              :min-date="minDate"
+              :max-date="maxDate"
+              :formatter="formatter"
+              @confirm="onConfirmTime"
+              @cancel="showDate = false"
+            />
+          </Popup>
         </div>
 
-        <Popup
-          v-model="showDate"
-          position="bottom"
-        >
-          <DatetimePicker
-            v-model="currentDate"
-            type="year-month"
-            title="选择年月"
-            :min-date="minDate"
-            :max-date="maxDate"
-            :formatter="formatter"
-            @confirm="onConfirmTime"
-            @cancel="showDate = false"
-          />
-        </Popup>
-      </div>
-
-      <!-- 红包奖励 -->
-      <div class="red-content" v-if="actType == 2">
-        <div class="flex_between red-price">
-          <div class="">
-            <div class="flex_middle">
-              获得红包奖励总额(元)
-              <img class="ask-icon" @click="showRedDesc = true" :src="getImgUrl('publicMobile/game/withdrawal/history_desc_ask.png')" />
+        <!-- 红包奖励 -->
+        <div class="red-content" v-if="actType == 2 && list.length">
+          <div class="flex_between red-price">
+            <div class="">
+              <div class="flex_middle">
+                获得红包奖励总额(元)
+                <img class="ask-icon" @click="showRedDesc = true" :src="getImgUrl('publicMobile/game/withdrawal/history_desc_ask.png')" />
+              </div>
+              <div class="red-price-text">{{totalPrize || 0}}</div>
             </div>
-            <div class="red-price-text">88.8</div>
+            <img class="withdrawal-icon" @click="onWithdrawal" :src="getImgUrl('publicMobile/game/withdrawal/history_withdrawal.png')" />
           </div>
-          <img class="withdrawal-icon" @click="onWithdrawal" :src="getImgUrl('publicMobile/game/withdrawal/history_withdrawal.png')" />
-        </div>
 
-        <div class="red-list">
-          <div class="flex_between list-item">
-            <div class="list-info">
-              <div>盖楼9层抽奖</div>
-              <div class="list-info-time">2021-01-01 10:00:00</div>
+          <div class="red-list">
+            <div class="flex_between list-item" v-for="item in list">
+              <div class="list-info">
+                <div>盖楼{{item.floor}}层抽奖</div>
+                <div class="list-info-time">{{item.timeText}}</div>
+              </div>
+              <div class="list-num">{{item.prizeText}}元</div>
             </div>
-            <div class="list-num">5.00元</div>
+            <div class="no-more" v-if="prizePage.page >= prizePage.totalPage">暂无更多~</div>
           </div>
-          <div class="no-more">暂无更多~</div>
-        </div>
-        <Popup
-          v-model="showRedDesc"
-          style="background-color: transparent"
-        >
-          <div class="red-desc-popup">
-            <div class="red-desc-content">
-              <img class="red-desc-back" :src="getImgUrl('publicMobile/game/withdrawal/history_desc.png')" />
-              <img class="red-desc-close" @click="showRedDesc = false" :src="getImgUrl('publicMobile/game/withdrawal/history_desc_close.png')" />
-              <div class="red-desc-box">
-                <div class="red-desc-title">温馨提示</div>
-                <div class="red-desc-text">
-                  1、对该活动抽奖获得的奖励：现金红包可以提现到支付宝账户，用户提现前需绑定支付宝账户，前3次不限金额提现，后面需要按照1的整数倍提现（提现按钮分别为:全部提现（仅可提现3次）、1元、5元、10元、20元、50元、100元、自定义金额）
-                  <br />
-                  <br />
-                  2、中奖用户请可以根据需求提现到个人的付宝账户，每个支付宝账号仅限一人使用
+          <Popup
+            v-model="showRedDesc"
+            style="background-color: transparent"
+          >
+            <div class="red-desc-popup">
+              <div class="red-desc-content">
+                <img class="red-desc-back" :src="getImgUrl('publicMobile/game/withdrawal/history_desc.png')" />
+                <img class="red-desc-close" @click="showRedDesc = false" :src="getImgUrl('publicMobile/game/withdrawal/history_desc_close.png')" />
+                <div class="red-desc-box">
+                  <div class="red-desc-title">温馨提示</div>
+                  <div class="red-desc-text">
+                    1、对该活动抽奖获得的奖励：现金红包可以提现到支付宝账户，用户提现前需绑定支付宝账户，前3次不限金额提现，后面需要按照1的整数倍提现（提现按钮分别为:全部提现（仅可提现3次）、1元、5元、10元、20元、50元、100元、自定义金额）
+                    <br />
+                    <br />
+                    2、中奖用户请可以根据需求提现到个人的付宝账户，每个支付宝账号仅限一人使用
+                  </div>
                 </div>
               </div>
             </div>
+          </Popup>
+        </div>
+        
+        <!-- 空数据 -->
+        <div class="nodata" v-if="list.length == 0">
+          <NoData
+            :icon="`${actType == 1 ? 'user' : 'content'}`"
+            :title="`${actType == 1 ? '暂无邀请用户记录~' : '暂无任何奖品记录~'}`"
+          />
+          <div class="tips">
+            温馨提示： 
+            <br />
+            当日奖励的所有盖楼机会需在24小时内用完，逾期不候
           </div>
-        </Popup>
-      </div>
-      
-      <!-- 空数据 -->
-      <div class="nodata" v-if="list.length == 0">
-        <NoData
-          :icon="`${actType == 1 ? 'user' : 'content'}`"
-          :title="`${actType == 1 ? '暂无邀请用户记录~' : '暂无任何奖品记录~'}`"
-        />
-        <div class="tips">
-          温馨提示： 
-          <br />
-          当日奖励的所有盖楼机会需在24小时内用完，逾期不候
+          <!-- <div class="footer-banner" v-if="actType == 1">
+            <img class="footer-banner-img" :src="getImgUrl('publicMobile/game/withdrawal/start_task.png')" />
+            <div class="footer-banner-btn" />
+          </div> -->
+          <div class="footer-banner">
+            <img class="footer-banner-img" :src="getImgUrl('publicMobile/game/withdrawal/invite.png')" />
+            <div class="footer-banner-btn" />
+          </div>
         </div>
-        <div class="footer-banner" v-if="actType == 1">
-          <img class="footer-banner-img" :src="getImgUrl('publicMobile/game/withdrawal/start_task.png')" />
-          <div class="footer-banner-btn" />
-        </div>
-        <div class="footer-banner" v-if="actType == 2">
-          <img class="footer-banner-img" :src="getImgUrl('publicMobile/game/withdrawal/invite.png')" />
-          <div class="footer-banner-btn" />
-        </div>
-      </div>
+      </List>
     </div>
   </div>
 </template>
 
 <script>
-import { DatetimePicker, Popup, Dialog } from 'vant';
-import { getImgUrl } from '@/utils/tools';
+import { DatetimePicker, Popup, Dialog, List } from 'vant';
+import Dayjs from 'dayjs';
 import NoData from '@/components/nodata'
+import { getImgUrl, storage } from '@/utils/tools';
+import gameApi from '@/apis/game';
+
+let defToken = 'AQQAAAAAYfGMBhO1r6h85uACdaberb2ahlPFSv7KDBSE6JBgxdLkvYpcDoWnCKpMd4o=';
+const defRankPage = {
+  next: 0,
+  size: 20,
+  hasNext: false,
+};
+const defPrizePage = {
+  page: 1,
+  size: 20,
+  totalPage: 1
+};
 
 export default {
   data() {
     return {
+      activityId: storage.get('buildingGameId') || '45',
+      token: storage.get('token') || defToken,
       actType: 1,
-      list: [{}, {}],
+      list: [],
       showDate: false,
       showRedDesc: false,
       minDate: new Date(2022, 0, 1),
       maxDate: new Date(2022, 2, 1),
       currentDate: new Date(),
+      listLoading: false,
+      listFinished: false,
+      // 红包奖励
+      totalPrize: 0,
+      prizePage: {
+        ...defPrizePage,
+      },
+      rankPage: {
+        ...defRankPage,
+      },
+      searchTime: Dayjs(new Date()).format('YYYY-MM')
     };
   },
   components: {
     NoData,
     DatetimePicker,
     Popup,
+    List,
   },
   mounted () {
+    let {
+      type
+    } = this.$router.history.current.query;
+    this.listLoading = false;
+    if(!!type) {
+      this.actType = +type;
+    }
+    this.getData();
   },
   methods: {
     getImgUrl,
     onChangeBar(type) {
       if(type != this.actType) {
-        this.actType = type
+        this.actType = type;
+        this.getData();
+      }
+    },
+    // 获取数据
+    getData() {
+      this.listFinished = false;
+      if(this.actType == 1) {
+        this.rankPage = {
+          ...defRankPage
+        };
+        this.getRankList(true);
+      } else {
+        this.prizePage = {
+          ...defPrizePage
+        };
+        this.getTotalPrize();
+        this.getPrizeList(true);
+      }
+    },
+    // 获取机会明细列表
+    getRankList(frist) {
+      gameApi.getRankList({
+        configId: this.activityId,
+        searchTime: this.searchTime,
+        next: this.rankPage.next,
+        size: this.rankPage.size,
+      }, {
+        token: this.token,
+        showLoading: false,
+      }).then(res => {
+        if(res.code == 0) {
+          const data = res.data;
+          this.rankPage.hasNext = data.hasNext;
+          this.rankPage.next = data.next;
+          if(!data.hasNext) {
+            this.listFinished = true;
+          }
+          // data.records.forEach(item => {
+          //   item.timeText = Dayjs(item.drawTime * 1000).format('YYYY-MM-DD HH:mm:ss');
+          //   item.prizeText = parseFloat(item.prize).toFixed(2);
+          // });
+          if(frist) {
+            this.list = data.records;
+          } else {
+            this.list = this.list.concat(data.records);
+          }
+        }
+      }).catch(res => {
+        this.listLoading = false;
+      });
+    },
+    // 总奖励
+    getTotalPrize() {
+      gameApi.getTotalPrice({}, {
+        token: this.token
+      }).then(res => {
+        if(res.code == 0) {
+          this.totalPrize= res.data.prize
+        }
+      });
+    },
+    // 奖励数据列表
+    getPrizeList(frist) {
+      gameApi.getPriceList({
+        activityId: this.activityId,
+        page: this.prizePage.page,
+        size: this.prizePage.size,
+      }, {
+        token: this.token,
+        showLoading: false,
+      }).then(res => {
+        if(res.code == 0) {
+          const data = res.data;
+          this.prizePage.totalPage = data.totalPages;
+          data.records.forEach(item => {
+            item.timeText = Dayjs(item.drawTime * 1000).format('YYYY-MM-DD HH:mm:ss');
+            item.prizeText = parseFloat(item.prize).toFixed(2);
+          });
+          if(data.page >= data.totalPages) {
+            this.listFinished = true;
+          }
+          if(frist) {
+            this.list = data.records;
+          } else {
+            this.list = this.list.concat(data.records);
+          }
+        }
+      }).catch(res => {
+        this.listLoading = false;
+      });
+    },
+    handleBottom() {
+      if(this.actType == 1 && this.rankPage.hasNext) {
+        this.getRankList();
+      } else if(this.actType == 2 && this.prizePage.page < this.prizePage.totalPage) {
+        this.prizePage.page += 1;
+        this.getPrizeList();
       }
     },
     formatter(type, val) {
@@ -150,19 +292,22 @@ export default {
       return val;
     },
     onConfirmTime(date) {
-      console.log("🚀 ~ file: index.vue ~ line 101 ~ onConfirmTime ~ date", date)
+      this.searchTime = Dayjs(date).format('YYYY-MM')
+      console.log("🚀 ~ file: index.vue ~ line 294 ~ onConfirmTime ~ this.searchTime", this.searchTime)
       this.showDate = false;
     },
     onWithdrawal() {
-      Dialog.confirm({
-        width: 280,
-        title: '提示',
-        message: '你的账户尚未绑定支付宝，请先完成支付宝账号再操作',
-      }).then(() => {
-          // on confirm
-      }).catch(() => {
-        // on cancel
-      });
+      // this.totalPrize
+      console.log("🚀 ~ file: index.vue ~ line 301 ~ onWithdrawal ~ this.totalPrize", this.totalPrize)
+      // Dialog.confirm({
+      //   width: 280,
+      //   title: '提示',
+      //   message: '你的账户尚未绑定支付宝，请先完成支付宝账号再操作',
+      // }).then(() => {
+      //     // on confirm
+      // }).catch(() => {
+      //   // on cancel
+      // });
     },
   },
 };
@@ -170,8 +315,11 @@ export default {
 
 <style lang="scss" scoped>
   .history-box {
-    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
     background-color: #F5F5F5;
+    overflow: hidden;
   }
   .bar-list {
     position: relative;
@@ -205,9 +353,19 @@ export default {
   }
 
   .history-content {
-    margin-top: 8px;
+    flex: 1;
+    // height: 300px;
+    overflow: auto;
   }
 
+  .opportunity-content,
+  .red-content {
+    height: 100%;
+    padding-top: 8px;
+  }
+  .opportunity-list {
+    height: 100%;
+  }
   .opportunity-time {
     height: 48px;
     font-size: 17px;
@@ -231,6 +389,9 @@ export default {
     padding: 3px 30px 0;
     background-color: #fff;
   }
+  .red-list {
+    min-height: calc(100% - 112px);
+  }
   .list-item {
     font-size: 12px;
     font-weight: 600;
@@ -251,6 +412,9 @@ export default {
     color: #E5352F;
     line-height: 20px;
   }
+  .list-num-def {
+    color: #999;
+  }
   .no-more {
     font-size: 14px;
     color: #999999;
@@ -265,7 +429,6 @@ export default {
     color: #333333;
     line-height: 22px;
     padding: 12px 0 12px 16px;
-    margin-top: 8px;
     margin-bottom: 8px;
     background-color: #fff;
   }
