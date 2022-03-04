77e38592ca5b3e066e99e05b590eb9e296769d43
huyueke <huyueke@yongdaxing.cn>
2022-03-04 10:08:26 +0800

    fix

---
+++ src/views/agreement/components/store/5.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+<p><strong>约购</strong><strong>店铺注销协议</strong></p>
+<p><strong><strong><br /></strong></strong> 尊敬的用户，感谢您长久以来对约购的支持，在您决定注销您的约购店铺之前，请您认真阅读、理解并同意本协议。本协议由您与约购平台共同缔结，本协议具有合同效力。<br /><strong><strong>第一：店铺注销类型</strong></strong><br />1.&nbsp;每次仅可注销一个店铺。<br />2.&nbsp;个人店铺注销：当您申请注销的约购店铺主体为个人店铺时，应由您本人向约购提出注销申请。<br />3.&nbsp;企业店铺注销：当您申请注销的约购店铺主体为企业店铺时，应由企业法定代表人或企业指定授权代表人向约购提出注销申请，不符合上述要求的，您将无法申请注销约购店铺。<br /><strong><strong>第二：注销条件</strong></strong><br />在您申请注销店铺前，您应确保您有权决定处理该店铺的注销事宜，不侵犯任何第三方的合法权益，如因此引发任何投诉争议，应由您自行承担全部责任。符合上述要求的情况下，你还需满足下述全部条件店铺才能申请注销：<br />1.&nbsp;店铺有交易的，注销申请应在店铺最后一笔交易完成/关闭后的三个月以后（含三个月）；<br />2.&nbsp;店铺没有未处理完的交易纠纷；<br />3.&nbsp;店铺没有任何处罚记录；<br />4.&nbsp;店铺账户内无余额；<br />5.&nbsp;店铺商品全部已下架；<br />6.&nbsp;总部店铺下无分店。<br /><strong><strong>第三：注销流程</strong></strong><br />您应当登录约购app端操作店铺注销流程。如您的店铺符合本协议注销条件，则您应按照约购提示要求上传相应的注销材料，约购将在30个工作日内进行审核。您承诺该店铺注销申请一经提交，您不会以任何理由要求约购予以撤销。<br /><strong><strong>第四：您同意</strong></strong><br />如您申请注销约购店铺时，约购尚在或需要对您的有关交易或账号进行核查的，约购有权继续冻结您的约购店铺或店铺内全部或部分资金以便继续核查，并在必要时有权向监管或司法部门举报欺诈或非法行为。<br /><strong><strong>第五：您了解并知悉</strong></strong><strong><strong>约购</strong></strong><strong><strong>店铺注销后，店铺内所有的历史记录信息将被清除且无法恢复，店铺购买的增值服务同时失效且不予退款。</strong></strong><strong><strong><br /></strong></strong><strong><strong>第六：本协议未尽事宜，参照《</strong></strong><strong><strong>约购</strong></strong><strong><strong>平台</strong></strong><strong><strong>用户</strong></strong><strong><strong>服务协议》执行。</strong></strong><br />店铺注销后，所有店铺相关信息丢失，店铺购买的增值服务和应用失效且不予退款，请谨慎操作！<br /><br />我已仔细阅读并同意，下一步</p>
+<p><br /><br /></p></div>
+</template>
+<style lang="scss" scoped>
+p {
+  text-indent:2em;
+  text-align: justify;
+  margin-bottom: 1em;
+}
+p:nth-child(1) {
+  text-align: center;
+  text-indent:0
+}
+</style>
