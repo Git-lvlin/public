5f6bedd9bcf0da55858f92016b2719725c852998
zengweigang <zengweigang@yongdaxing.cn>
2022-07-28 14:52:02 +0800

    pdf

--- src/views/escrow-agreement/index.vue
+++ src/views/escrow-agreement/index.vue
@@ -1,17 +1,32 @@
 <template>
-  <div>
-    托管协议
+  <div class="page">
+    <div id="demo"></div>
   </div>
 </template>
 
 <script>
-
+import Pdfh5 from "pdfh5";
+import "pdfh5/css/pdfh5.css";
 export default {
-  
+  mounted() {
+    this.pdfh5 = new Pdfh5("#demo", {
+      backTop: false,
+      pageNum: false,
+      pdfurl: 'https://pro-yeahgo-oss.yeahgo.com/publicMobile/make-money/pdf.pdf'
+    })
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 
+.page {
+  background: #fff;
+  height: 100vh;
+  overflow: hidden;
+}
 
+#demo {
+  height: 100vh;
+}
 </style>
