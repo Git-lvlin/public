09fec557429eb685436fa4f9a8036488ed7ddd06
zengweigang <zengweigang@yongdaxing.cn>
2022-08-03 10:43:56 +0800

    share

--- src/views/make-money-share/index.vue
+++ src/views/make-money-share/index.vue
@@ -3,16 +3,23 @@
     <div class="box">
       <ul>
         <li>
-          <div class="unit">
-            <span>1</span>
-            <div>注册约购APP</div>
+          <div>
+            <div class="unit">
+              <span>1</span>
+              <div>注册约购APP</div>
+            </div>
+            <div class="btn" @click="go">立即注册</div>
           </div>
         </li>
         <li>
-          <div class="unit">
-            <span>2</span>
-            <div>下单购买氢原子商品并签署<br />在线合同</div>
+          <div>
+            <div class="unit">
+              <span>2</span>
+              <div>下单购买氢原子商品并签署<br />在线合同</div>
+            </div>
+            <div v-if="link" class="buy" @click="go">立即购买</div>
           </div>
+
         </li>
         <li>
           <div class="unit">
@@ -22,27 +29,34 @@
           <div class="sub">在"约购APP-我的-我的服务-店主入驻"<br />处申请入驻</div>
         </li>
       </ul>
-      <div class="btn" @click="go">立即注册参与</div>
+      <!-- <div class="btn" @click="go">立即注册参与</div> -->
     </div>
   </div>
 </template>
 
 <script>
 import { getImgUrl } from '@/utils/tools';
+import generateurllink from '@/apis/make-money-share';
 
 export default {
   data() {
-    return {};
+    return {
+      link: ''
+    };
   },
-  // created() {
-  //   document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
-  //     WeixinJSBridge.call('hideOptionMenu');
-  //   }); 
-  // },
+  created() {
+    generateurllink({ inviteCode: this.$route.query.inviteCode })
+      .then(res => {
+        this.link = res.data.goodsdetail
+      })
+  },
   methods: {
     getImgUrl,
+    buy() {
+      window.location.href = this.link;
+    },
     go() {
-      this.$router.push({ path: '/web/new-share', query: { ...this.$route.query}})
+      this.$router.push({ path: '/web/new-share', query: { ...this.$route.query } })
     },
   },
 };
@@ -102,15 +116,14 @@ export default {
   }
 
   .btn {
-    width: 170px;
-    line-height: 36px;
-    background: #FF0000;
-    border-radius: 4px 4px 4px 4px;
+    width: 100px;
+    line-height: 34px;
+    background: #F76122;
+    border-radius: 0px 0px 0px 0px;
     text-align: center;
-    font-size: 14px;
-    font-weight: 400;
-    color: #FFFFFF;
-    margin: 0 auto;
+    color: #fff;
+    margin-left: 38px;
+    margin-top: 10px;
   }
 }
 </style>
